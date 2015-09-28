78ac29f00f4ec77aae5ec9ab6674bb2f73c10788
simjxu <simjxu@gmail.com>
2015-09-27 21:59:32 -0700

    Added section on pulling code from Github

--- Github_stuff.m
+++ Github_stuff.m
@@ -1,5 +1,31 @@
 %% Sample MATLAB code editing
 
+%% Pulling the code from Github
+% I want to pull the code into my working directory
+
+%{
+1. In a new working directory I've just created, I have nothing there 
+(mine is C:\Users\Simon\Github-machinelearning)
+
+2. Open the Git command line, and change directory to your working folder:
+cd C:\Users\Simon\Github-machinelearning
+(press enter)
+
+3. Initialize your working folder as a git folder:
+git init
+(this creates a hidden folder called ".git" in your working folder)
+
+4. Direct the connection of Git to our github repository:
+git remote add origin https://github.com/cs229-ML/condition-monitoring.git
+
+5. Pull all the updated code into your local working directory:
+git pull origin master
+
+5a. Now all the files from github are in your working directory, and it is
+up to date.
+
+%}
+
 %% Make a first "commit" to a code
 % I've just started writing the code, and I am going to save this code onto
 % Github.
@@ -10,13 +36,14 @@ I have this file, named "Github_stuff.m"
 
 2. I open the Git command line, and navigate to my directory by typing:
 cd C:\Users\Simon\Github-machinelearning
-(press enter)
 
 3. Add the file onto Git locally:
 git add Github_stuff.m
 
-4. Commit the file:
+4. Commit the file, with a note in quotes indicating what you've changed:
 git commit -m "Initial commit of a MATLAB file"
+
+(everytime you commit, a history line is stored)
 
 5. Indicate the destination of the file:
 git remote add origin https://github.com/cs229-ML/condition-monitoring.git
@@ -28,8 +55,5 @@ git push -u origin master
 
 7. You are prompted for your github username and password. Since you guys
 are admins on this project, your username and password will allow the push.
-
-
-
 
 %}
