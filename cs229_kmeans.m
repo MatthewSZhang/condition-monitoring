e0953c3b5f5484d07672534faadbda58fb18ac70
Cheryl Danner <cdanner@stanford.edu>
2015-11-06 22:27:07 -0800

    Update kmeans to work on a single structure instead of requiring two

--- cs229_kmeans.m
+++ cs229_kmeans.m
@@ -1,10 +1,16 @@
-function [cluster_ids, centroids] = cs229_kmeans(k, m3_features, m4_features)
+function [cluster_ids, centroids] = cs229_kmeans(k, feat_struct)
+%CS229_KMEANS is pretty self explanatory
 % I couldn't think of what to name this function other than kmeans, but 
 % that name was already taken!
+%
+% Usage:
+%   [cluster_ids, centroids] = cs229_kmeans(k, feat_struct);
+% 
+% Note: plot after clustering won't work unless feat_struct has features
+%   time_delta_days and rms_x_acc
 
 % reformat for use in learning algorithms
-feat_mat = [struct2mat(remove_time_stamp(m3_features)); 
-    struct2mat(remove_time_stamp(m4_features))];
+feat_mat = struct2mat(remove_time_stamp(feat_struct)); 
 
 % run kmeans
 rng(1); % for reproducibility
@@ -15,16 +21,18 @@ for icluster = 1:k
     fprintf(1, 'Number of points in cluster %d: %d\n', icluster, sum(cluster_ids == icluster));
 end
 
+if ~isfield(feat_struct, 'time_delta_days') && isfield(feat_struct, 'rms_x_acc')
+    return;
+end
 % plot some data color-coded by cluster
 figure('Color', 'w');
 cmap = colormap('lines'); hold on;
-time_delta = [m3_features.time_delta_days', m4_features.time_delta_days'];
-rms_x_acc = [m3_features.rms_x_acc, m4_features.rms_x_acc];
 for icluster = 1:k
     inds = cluster_ids == icluster;
-    plot(time_delta(inds), rms_x_acc(inds), 'Color', cmap(icluster, :), ...
-        'Marker', '*', 'LineStyle', 'None');
+    plot(feat_struct.time_delta_days(inds), feat_struct.rms_x_acc(inds), ...
+        'Color', cmap(icluster, :), 'Marker', '*', 'LineStyle', 'None');
 end
-grid on;
+grid on; xlabel('Time (days)'); ylabel('RMS X Acceleration');
+legend(num2str((1:k)'));
+title(sprintf('Kmeans Categorization (k = %d)', k));
 
-disp(' '); % this is just somewhere to give me a breakpoint
